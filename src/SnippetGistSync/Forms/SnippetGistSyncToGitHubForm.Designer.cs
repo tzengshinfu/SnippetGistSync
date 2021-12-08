f47b0ee3181e4a721b0092ebf0f8cb42f52415ba
tzengshinfu <tzengshinfu@gmail.com>
2021-12-08 13:43:50 +0800

    修正:替換元件WebView2→WebBrowser

--- src/SnippetGistSync/Forms/SnippetGistSyncToGitHubForm.Designer.cs
+++ src/SnippetGistSync/Forms/SnippetGistSyncToGitHubForm.Designer.cs
@@ -24,29 +24,26 @@ namespace SnippetGistSync {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
-            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
-            // 
-            // webView21
-            // 
-            this.webView21.CreationProperties = null;
-            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webView21.Location = new System.Drawing.Point(0, 0);
-            this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(752, 477);
-            this.webView21.Source = new System.Uri("https://github.com/settings/tokens/new", System.UriKind.Absolute);
-            this.webView21.TabIndex = 1;
-            this.webView21.ZoomFactor = 1D;
-            this.webView21.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView21_NavigationCompleted);
-            // 
+            //
+            // webBrowser1
+            //
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(752, 477);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://github.com/settings/tokens/new", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            //
             // SnippetGistSyncToGitHubForm
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(752, 477);
-            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.webBrowser1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "SnippetGistSyncToGitHubForm";
             this.ShowIcon = false;
@@ -54,13 +51,12 @@ namespace SnippetGistSync {
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "到 GitHub";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SnippetGistSyncToGitHubForm_FormClosing);
-            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
--- src/SnippetGistSync/Forms/SnippetGistSyncToGitHubForm.cs
+++ src/SnippetGistSync/Forms/SnippetGistSyncToGitHubForm.cs
@@ -5,16 +5,23 @@ namespace SnippetGistSync {
     public partial class SnippetGistSyncToGitHubForm : Form {
         public SnippetGistSyncToGitHubForm() {
             InitializeComponent();
+
+            webBrowser1.ScriptErrorsSuppressed = true;
+
         }
 
-        private void webView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) {
-            if (webView21.Source == new Uri("https://github.com/settings/tokens/new")) {
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) {
+            if (webBrowser1.ReadyState < WebBrowserReadyState.Complete) {
+                return;
+            }
+
+            if (webBrowser1.Url == new Uri("https://github.com/settings/tokens/new")) {
                 MessageBox.Show($"一、[scopes]請選擇'gist'。{Environment.NewLine}二、產生token後複製到剪貼簿並關閉GitHub視窗。{Environment.NewLine}三、將token貼到'Personal access token'欄位。{Environment.NewLine}四、按'儲存'按鈕。");
             }
         }
 
         private void SnippetGistSyncToGitHubForm_FormClosing(object sender, FormClosingEventArgs e) {
-            webView21.CoreWebView2.CookieManager.DeleteAllCookies();
+            webBrowser1.Navigate("javascript:void((function(){var a,b,c,e,f;f=0;a=document.cookie.split('; ');for(e=0;e<a.length&&a[e];e++){f++;for(b='.'+location.host;b;b=b.replace(/^(?:%5C.|[^%5C.]+)/,'')){for(c=location.pathname;c;c=c.replace(/.$/,'')){document.cookie=(a[e]+'; domain='+b+'; path='+c+'; expires='+new Date((new Date()).getTime()-1e11).toGMTString());}}}})())"); //清除Cookie，參考來源：https://stackoverflow.com/a/1234299
         }
     }
 }
